e7b36e448329738b4164a97c1784c7eb557aae41
Marcilino da Maia <73150487+marclnomaia@users.noreply.github.com>
2023-01-31 16:23:50 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+
+            Console.WriteLine("type a word or phrase!");
+            word = Console.ReadLine();
+            int x = word.Length - 1;
+
+            while (x != -1)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == word[x])
+                    {
+                        if (x == 0)
+                        {
+                            Console.WriteLine("E Palindrome");
+                            Console.ReadLine();
+                        }
+                        x--;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nao e Palindrome!");
+                        i = word.Length;
+                        x = -1;
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
